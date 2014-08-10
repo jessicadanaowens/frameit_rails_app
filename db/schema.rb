3f74218c4da65328deff1ea52dfc8998029ba50e
Jessica Owens <jessicadanaowens@gmail.com>
2014-08-10 09:07:36 -0600

    added guest column to users table with boolean

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140808175344) do
+ActiveRecord::Schema.define(version: 20140810150627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,9 +23,12 @@ ActiveRecord::Schema.define(version: 20140808175344) do
   end
 
   create_table "users", force: true do |t|
-    t.string "email"
-    t.string "password_hash"
-    t.string "password_salt"
+    t.string  "email"
+    t.string  "password_hash"
+    t.string  "password_salt"
+    t.string  "first_name"
+    t.string  "last_name"
+    t.boolean "guest"
   end
 
 end
