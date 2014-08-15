28563dd4805b520e032fa92e32ad22d083e2a4eb
Jessica Owens <jessicadanaowens@gmail.com>
2014-08-15 14:40:36 -0600

    wrote to test to check that picture description appears on page after user submits the form

--- spec/features/upload_photo_spec.rb
+++ spec/features/upload_photo_spec.rb
@@ -18,8 +18,15 @@ feature "User can upload a photo" do
 
     click_on "Upload a picture to frame"
 
+    fill_in "picture_file_name", :with => "description"
     attach_file('picture_avatar', 'spec/photos/frame.png')
     click_button "Upload a picture to frame"
+
+    expect(page).to have_content "description"
+  end
+
+  def edit
+
   end
 
 end
