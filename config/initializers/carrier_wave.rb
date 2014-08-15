f9a3f11901509383f63252294ee4f6e21e58e4fe
Jessica Owens <jessicadanaowens@gmail.com>
2014-08-15 16:22:56 -0600

    dont upload to s3 in test

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,9 +1,15 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',                        # required
-    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],                        # required
-    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],                        # required
-    :region  => 'us-west-1'
+    :provider => 'AWS', # required
+    :aws_access_key_id => ENV["AWS_ACCESS_KEY_ID"], # required
+    :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"], # required
+    :region => 'us-west-1'
   }
-  config.fog_directory  = "frame-it-#{Rails.env}"
+  config.fog_directory = "frame-it-#{Rails.env}"
+
+  if Rails.env.test?
+    config.storage = :file
+  else
+    config.storage = :fog
+  end
 end
