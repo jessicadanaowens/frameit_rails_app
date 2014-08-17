f3c021a89f8e3cc46e4daf425783b1a7cb4b92fd
Jessica Owens <jessicadanaowens@gmail.com>
2014-08-17 12:12:31 -0600

    created a functioning jquery slider on the pictures show page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -59,4 +59,39 @@ $(document).ready(function() {
     }
   });
 
+  //jquery slideshow
+
+  //configuration
+  var width = 720;
+  var animationSpeed = 500;
+  var pause = 1000;
+  var currentSlide = 1;
+
+  //cache DOM
+  var $slider = $('#slider');
+  var $slideContainer = $slider.find('.slides');
+  var $slides = $slideContainer.find('.slide');
+
+  var interval;
+
+  function startSlider(){
+    interval = setInterval(function() {
+        $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+          currentSlide++;
+          if(currentSlide === $slides.length) {
+            currentSlide = 1;
+            $slideContainer.css('margin-left', 0);
+          }
+        });
+      }, pause);
+  }
+
+  function stopSlider() {
+    clearInterval(interval);
+  }
+
+  $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+
+  startSlider();
+
 });
