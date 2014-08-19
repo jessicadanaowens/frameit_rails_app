c18d45c503b57a36b670cfa94a0bde6a01ebd2d0
Jessica Owens <jessicadanaowens@gmail.com>
2014-08-18 18:35:50 -0600

    added flash message styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -94,4 +94,16 @@ $(document).ready(function() {
 
   startSlider();
 
+  //alert messages
+
+  $(function () {
+    var alert = $('.alert');
+    if (alert.length > 0) {
+      alert.show().animate({height: alert.outerHeight()}, 200);
+      window.setTimeout(function() {
+        alert.slideUp();
+      }, 3000);
+    }
+  });
+
 });
