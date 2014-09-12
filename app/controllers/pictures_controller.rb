e8320a425f91c9661aed055be6ae7602dd3c270d
Jessica Owens <jessicadanaowens@gmail.com>
2014-09-12 11:48:24 -0600

    removed image class from pictures controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -50,48 +50,6 @@ class PicturesController < ApplicationController
 
   def show
     @picture = Picture.find(params[:id])
-    canvas_width = 200
-    picture_width = @picture.width.to_i
-    @pixels_per_inch = canvas_width / picture_width
-    @canvas_height = @picture.height.to_i * @pixels_per_inch
-    @picture_height = @picture.height.to_i * @pixels_per_inch
-  end
-
-  def image
-    # FIXME: This needs to grab a real URL
-    # FIXME: This belongs in its own controller
-    RVG::dpi = 72
-    frame_pieces = Image.read(Rails.root.join("app", "assets", "images", "ScallopedBorder.png"))
-    rvg = RVG.new(5.in, 5.in) do |canvas|
-      canvas.background_fill = 'white'
-
-      canvas.g do |top_left|
-        frame_pieces.each do |frame_piece|
-          top_left.image(frame_piece)
-        end
-      end
-
-      canvas.g.translate(2.5.in, 2.5.in - 1).rotate(-90) do |top_right|
-        frame_pieces.each do |frame_piece|
-          top_right.image(frame_piece)
-        end
-      end
-
-      canvas.g.translate(5.in - 1, 2.5.in).rotate(90) do |bottom_right|
-        frame_pieces.each do |frame_piece|
-          bottom_right.image(frame_piece)
-        end
-      end
-
-      canvas.g.translate(2.5.in - 1, 5.in - 1).rotate(180) do |bottom_left|
-        frame_pieces.each do |frame_piece|
-          bottom_left.image(frame_piece)
-        end
-      end
-    end
-    img = rvg.draw
-    img.format = params[:format].upcase
-    send_data img.to_blob
   end
 
   def share_picture
