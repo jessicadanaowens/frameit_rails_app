73fc01915084690a14211bc1b7468f1a90190b4d
Jessica Owens <jessicadanaowens@gmail.com>
2014-09-12 10:34:22 -0600

    added some styling to picture show page

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process :resize_to_fit => [200, 200]
+  process :resize_to_fit => [400, 400]
 
   version :thumb do
     process :resize_to_fit => [50, 50]
